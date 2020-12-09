9091b2f06f0003bbdab98516bcc4fb79067835d2
Gurumurthy <317117@cognizant.com>
2020-12-09 12:23:08 +0530

    Add project files.

---
+++ AutomaticAttendanceTracker/EntityStore/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutomaticAttendanceTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutomaticAttendanceTracker.EntityStore
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ImageStore> ImageStore { get; set; }
+    }
+}
